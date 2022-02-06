635904b1c570760b4150eea5973846e36f2b37af
Alonso <f.vazquez151000@gmail.com>
2022-02-06 11:12:47 -0600

    :construction: Reto Final

--- RetoFInal/backend/core/__init__.py
+++ RetoFInal/backend/core/__init__.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
-
+"""
+Solo agrego algo para solucionar el commit xd
+"""
 app = FastAPI(
     title="Dashboard de divisas",
     description="Muesta la grafica en tiempo real de diversas divisas",
